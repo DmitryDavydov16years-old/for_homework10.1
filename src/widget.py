b17a28fb8749f9afb54571fab9b63a93680e0f32
dima <dima@gmail.com>
2024-04-09 16:14:14 +0300

    hello

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,14 @@
 from src.masks import masc_of_bill, masc_of_card
 
 
-def masks_of_cards(data: str) -> str:
+def masks_of_cards(time: str) -> str:
     """ "
     Функция переиспользует ранее написанные функции и возвращает исходную строку с замаскированным номером карты/счета.
     """
-    redact_data = data.split(" ")
-    if redact_data[0] != "Счет":
-        return f'{" ".join(redact_data[:-1])} {masc_of_card(redact_data[-1])}'
-    return f"Счет {masc_of_bill(redact_data[-1])}"
+    redact_time = time.split(" ")
+    if redact_time[0] != "Счет":
+        return f'{" ".join(redact_time[:-1])} {masc_of_card(redact_time[-1])}'
+    return f"Счет {masc_of_bill(redact_time[-1])}"
 
 
 def give_data(time: str) -> str:
