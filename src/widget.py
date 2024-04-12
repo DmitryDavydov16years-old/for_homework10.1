168a0bbc27dc275a4bf7e4e496c9c467a1244ce4
dima <dima@gmail.com>
2024-04-12 10:50:36 +0300

    finalli

--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,23 @@
 from src.masks import masking_account, masking_card
 
 
-def masking_card_or_account(time: str) -> str:
+def masking_card_or_account(card_or_account: str) -> str:
     """ "
     Функция переиспользует ранее написанные функции и возвращает исходную строку с замаскированным номером карты/счета.
     """
-    redact_time = time.split(" ")
-    if redact_time[0] != "Счет":
-        return f'{" ".join(redact_time[:-1])} {masking_card(redact_time[-1])}'
-    return f"Счет {masking_account(redact_time[-1])}"
+    split_card_or_account = card_or_account.split(" ")
+    if split_card_or_account[0] != "Счет":
+        return f'{" ".join(split_card_or_account[:-1])} {masking_card(split_card_or_account[-1])}'
+    return f"Счет {masking_account(split_card_or_account[-1])}"
 
 
-def changes_date_line(time: str) -> str:
+def changes_date_line(date: str) -> str:
     """
     Принимает на вход строку вида 2018-07-11T02:26:18.671407
     и возвращает строку с датой в виде 11.07.2018.
     """
-    redact_time = time.split("T")[0].split("-")
-    return f"{redact_time[2]}.{redact_time[1]}.{redact_time[0]}"
+    split_date = date.split("T")[0].split("-")
+    return f"{split_date[2]}.{split_date[1]}.{split_date[0]}"
 
 
 def replacing_email_addresses(line_with_text: str) -> str:
