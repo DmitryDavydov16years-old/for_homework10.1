40e9fab9e77ec05b061c65ef91cb7ba5ecfd3915
имя фамилия <dima@gmail.com>
2024-04-22 10:15:34 +0300

    finally

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,10 @@
 def sorting_the_dictionary(list_of_dictionaries: list, value_for_key: str = "EXECUTED") -> list:
+    """
+    функция принимает на вход список словарей и значение для
+    ключа state (опциональный параметр со значением по умолчанию EXECUTED)
+    и возвращает новый список, содержащий только те словари, у которых ключ
+    state содержит переданное в функцию значение
+    """
     modified_dictionary = []
     for dictionary in list_of_dictionaries:
         if dictionary["state"] == value_for_key:
@@ -7,5 +13,10 @@ def sorting_the_dictionary(list_of_dictionaries: list, value_for_key: str = "EXE
 
 
 def sorting_by_date(list_of_dictionaries: list, reverse: bool = True) -> list:
+    """"
+    Функция принимает на вход список словарей и возвращает новый список,
+    в котором исходные словари отсортированы по убыванию даты (ключ date).
+    Функция принимает два аргумента, второй необязательный задает порядок сортировки (убывание, возрастание).
+    """
     sorting = sorted(list_of_dictionaries, key=lambda x: x["date"], reverse=reverse)
     return sorting
