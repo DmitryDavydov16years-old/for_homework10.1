091fb50fea3a05b532cbeb84f7f7a9f3b4673195
имя фамилия <dima@gmail.com>
2024-05-17 11:21:53 +0300

    forever_finally_12.1

--- src/utils.py
+++ src/utils.py
@@ -5,12 +5,12 @@ from typing import Any
 import requests
 from dotenv import load_dotenv
 
-API_KEY = os.getenv("api_keys")
 load_dotenv()
+API_KEY = os.getenv("api_keys")
 
 
-def dictionaries_financial_transaction_data(path_to_file: str) -> Any:
-    """ "
+def finding_dictionaries_with_financial_transaction_data(path_to_file: str) -> Any:
+    """
     Функция, которая принимает на вход путь до JSON-файла и возвращает список словарей
      с данными о финансовых транзакциях. Если файл пустой, содержит не список или не найден,
      функция возвращает пустой список.
@@ -24,14 +24,14 @@ def dictionaries_financial_transaction_data(path_to_file: str) -> Any:
         return data
 
 
-def returns_transaction_amount(transaction: dict) -> Any:
-    """ "
+def finding_transaction_amount(transaction: dict) -> Any:
+    """ 
     Реализуйте функцию, которая принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях,
     возвращает тип float. Если транзакция была в USD или EUR, идет обращение к внешнему API для получения текущего
      курса валют и конвертации суммы операции в рубли.
     """
     currency_for_transaction = transaction["operationAmount"]["currency"]["code"]
-    url = f"https://v6.exchangerate-api.com/v6/0504d3f7601436eb90528abf/latest/{currency_for_transaction}"
+    url = f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/{currency_for_transaction}"
     response = requests.get(url, headers={"apikey": API_KEY})
     response_data = response.json()
     return float(transaction["operationAmount"]["amount"]) * response_data["conversion_rates"]["RUB"]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ import json
 from typing import Any
 from unittest.mock import patch
 
-from src.utils import dictionaries_financial_transaction_data, returns_transaction_amount
+from src.utils import finding_dictionaries_with_financial_transaction_data, finding_transaction_amount
 
 for_example = {
     "id": 667307132,
@@ -16,21 +16,21 @@ for_example = {
 
 
 @patch("requests.get")
-def test_returns_transaction_amount(mock_get: Any) -> None:
+def test_finding_dictionaries_with_financial_transaction_data(mock_get: Any) -> None:
     mock_get.return_value.json.return_value = {"conversion_rates": {"RUB": 90}}
     assert (
-        returns_transaction_amount(
-            {
-                "id": 667307132,
-                "state": "EXECUTED",
-                "date": "2019-07-13T18:51:29.313309",
-                "operationAmount": {"amount": "97853.86", "currency": {"name": "руб.", "code": "USD"}},
-                "description": "Перевод с карты на счет",
-                "from": "Maestro 1308795367077170",
-                "to": "Счет 96527012349577388612",
-            }
-        )
-        == 8806847.4
+            finding_transaction_amount(
+                {
+                    "id": 667307132,
+                    "state": "EXECUTED",
+                    "date": "2019-07-13T18:51:29.313309",
+                    "operationAmount": {"amount": "97853.86", "currency": {"name": "руб.", "code": "USD"}},
+                    "description": "Перевод с карты на счет",
+                    "from": "Maestro 1308795367077170",
+                    "to": "Счет 96527012349577388612",
+                }
+            )
+            == 8806847.4
     )
 
 
@@ -38,5 +38,5 @@ def test_dictionaries_financial_transaction_data() -> None:
     with patch("builtins.open") as mock_open:
         mock_file = mock_open.return_value.__enter__.return_value
         mock_file.read.return_value = json.dumps(for_example)
-        assert dictionaries_financial_transaction_data("../data/operations.json") == for_example
+        assert finding_dictionaries_with_financial_transaction_data("../data/operations.json") == for_example
         mock_open.assert_called_once_with("../data/operations.json", "r", encoding="UTF-8")
