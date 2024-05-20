df4efec4d02427d269586d40206da1302c06847b
имя фамилия <dima@gmail.com>
2024-05-20 17:03:53 +0300

    forever_finally_12.2

--- src/external_api.py
+++ src/external_api.py
@@ -1,4 +1,23 @@
+from typing import List
+
 from src.logger import setup_logging
 
 logger = setup_logging()
-logger.info("Все в порядке")
+
+
+# Я напишу здесь каку-нибудь функцию, чтоб тут был не только лог
+def plusOne(digits: List[int]) -> List[int]:
+    """
+    Функция принимает целое чсило в виде целочисленного массива цифр,
+    увеличвает это целое число на единицу и возвращает полученный массив цифр.
+    """
+
+    numbers = []
+    for number in digits:
+        numbers.append(str(number))
+    increased_number = int("".join(numbers)) + 1
+    new_array = []
+    for number in str(increased_number):
+        new_array.append(int(number))
+    logger.info("Все в порядке")
+    return new_array
